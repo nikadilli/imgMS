6d87ac4c6ec03376760d7838b383582d2e4cf8a9
nikadilli <nikadilli@gmail.com>
2021-03-30 18:06:13 +0200

    updated lod for new ts correction

--- imgMS/MSData.py
+++ imgMS/MSData.py
@@ -441,7 +441,7 @@ class MSData():
         """
         if self.corrected_TS is not None:
             ts_lod = pd.Series({el: self.lod[el] if el in self.lod.index else self.lod[el.replace(
-                ' oxide', '')] for el in self.corrected_TS.columns})
+                ' oxide', '')] for el in self.corrected_TS.columns}, name='LoD')
             self.corrected_TS = self.corrected_TS.append(ts_lod)
             for column in self.corrected_TS:
                 elem = element_formater(column, list(self.lod.index))
