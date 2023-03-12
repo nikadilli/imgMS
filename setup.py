19faa8ee5137845c4bdf6373ebb69eafefa6eca7
Veronika Faltusová <nika@Veronikas-MacBook-Pro.local>
2023-03-12 18:18:15 +0100

    adding docs for MSStats

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='imgMS',
-    version='0.2.0',
+    version='0.2.1',
     author='nikadilli',
     author_email='nikadilli@gmail.com',
     url="https://github.com/nikadilli/imgMS",
