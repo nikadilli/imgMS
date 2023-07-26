ef52e2b609c046b1f6ecc57a4a78006e78dab018
Veronika Faltusová <nikadilli@gmail.com>
2023-07-26 17:56:01 +0200

    improved interactive mean to calculate stats from all the measured elements

--- imgMS/MSStats.py
+++ imgMS/MSStats.py
@@ -13,6 +13,8 @@ import matplotlib.pyplot as plt
 
 import itertools
 
+stats_dict = {'mean':None, 'std':None, 'sum':None, 'min':None, 'max':None, 'med':None,}
+
 class interactive_average():
     """
     Creates interactive graph for the selection of the area of interest and
@@ -21,18 +23,28 @@ class interactive_average():
 
     Parameters
     ----------
-    elmap: elmap class from imgMS
-        Data of elemental image of one isotope stored as numpy array. 
+    msdata: MSData object from imgMS
+        Data of elemental imaging where stats will be calculated from a selected area of the elemental distribution image. 
     """
-    def __init__(self, elmap):
-        #TODO add possibility to pass multiple images / eg all isotopes of a
-        # map at once 
-        self.elmap = elmap
-
+    def __init__(self, msdata, elem=None):
+        
+        if elem is None: 
+            self.elem = msdata.isotope_names[0]
+        else: 
+            if elem in msdata.isotope_names:
+                self.elem = elem
+            else:
+                print(f'{elem} not in isotopes')
+        self.msdata = msdata
+        self.elmap = msdata.isotopes[self.elem].elmap
         self.get_attr()
+        self.stats_dict = { e:{'mean':None, 'std':None, 'sum':None, 'min':None, 'max':None, 'med':None,} for e in self.msdata.isotope_names }
         
 
     def get_attr(self):
+        """
+        get all the data required for interactive mean calculation for given map
+        """
         self.matrix = self.elmap.matrix
         self.shape = self.matrix.shape
         self.dx = self.elmap.dx
@@ -46,6 +58,18 @@ class interactive_average():
         self.array = np.array(self.matrix)
         self.array[np.isnan(self.array)] = 0
         
+    def switch_elem(self, elem):
+        """
+        Change the element for interactive calculation
+        Parameters
+        ----------
+        elem: str
+        Name of the element, which will be used for selecting the boundaries of area where stats are calculated.
+        """
+        self.elem = elem
+        self.elmap = self.msdata.isotopes[self.elem].elmap
+        self.get_attr()
+        
     def updateArray(self, array, indices):
         lin = np.arange(array.size)
         newArray = np.array(array.flatten())
@@ -53,29 +77,51 @@ class interactive_average():
         newArray[lin[indices]] = 1
         return newArray.reshape(array.shape)
     
-    def stats(self, array, indices):
-        lin = np.arange(array.size)
-        newArray = np.array(array.flatten())
-        self.mean = newArray[lin[indices]].mean()
-        self.std = newArray[lin[indices]].std()
-        self.sum = newArray[lin[indices]].sum()
-        self.min = newArray[lin[indices]].min()
-        self.max = newArray[lin[indices]].max()
-        self.med = np.median(newArray[lin[indices]])
-        print( f'mean: {self.mean}')
-        print( f'std: {self.std}')
-        print( f'sum: {self.sum}')
-        print( f'min: {self.min}')
-        print( f'max: {self.max}')
-        print( f'med: {self.med}')
+    def stats(self, indices):
+        """
+        Calculates the statistical values (mean, std, sum, minimum, 
+        maximum and median) from selected area of image.
+        Parameters
+        ----------
+        indices: list
+        List of bool to select which pixels are in the area.
+        """
+        for e in self.msdata.isotopes:
+            array = data.isotopes[e].elmap.matrix
+            lin = np.arange(array.size)
+            newArray = np.array(array.flatten())
+            mean = newArray[lin[indices]].mean()
+            std = newArray[lin[indices]].std()
+            suma = newArray[lin[indices]].sum()
+            mina = newArray[lin[indices]].min()
+            maxa = newArray[lin[indices]].max()
+            meda = np.median(newArray[lin[indices]])
+            
+            self.stats_dict[e]['mean'] = newArray[lin[indices]].mean()
+            self.stats_dict[e]['std'] = newArray[lin[indices]].std()
+            self.stats_dict[e]['sum'] = newArray[lin[indices]].sum()
+            self.stats_dict[e]['min'] = newArray[lin[indices]].min()
+            self.stats_dict[e]['max'] = newArray[lin[indices]].max()
+            self.stats_dict[e]['med'] = np.median(newArray[lin[indices]])
+            
+            print(e)
+            print( f'mean: {mean}')
+            print( f'std: {std}')
+            print( f'sum: {suma}')
+            print( f'min: {mina}')
+            print( f'max: {maxa}')
+            print( f'med: {meda}')
 
     def onSelect(self, verts):
+        """
+        Selection of area in the image.
+        """
         p = Path(verts)
         ind = p.contains_points(self.pix, radius=1)
         alpha = self.updateArray(self.array, ind)
         self.im.set_alpha(alpha)
         self.fig.canvas.draw_idle()
-        self.stats(self.array, ind)
+        self.stats(ind)
 
     def __call__(self, ax=None, vmax=None):
         self.ax = ax
@@ -91,30 +137,44 @@ class interactive_average():
         plt.show()
 
     def export_stats(self, filename, sheetname='Sheet1'):
-        
+        """
+        Export calculated stat for every isotope in the selected area 
+        into excel.
+        Parameters
+        ----------
+        filename: str
+        Excel file name to save data.
+        """
         workbook = xlsxwriter.Workbook(filename)
             
         wks1=workbook.add_worksheet(sheetname)
-
-        wks1.write(0,0,'mean')
-        wks1.write(1,0,'std')
-        wks1.write(2,0,'sum')
-        wks1.write(3,0,'min')
-        wks1.write(4,0,'max')
-        wks1.write(5,0,'med')
-
-        wks1.write(0,1,self.mean)
-        wks1.write(1,1,self.std)
-        wks1.write(2,1,self.sum)
-        wks1.write(3,1,self.min)
-        wks1.write(4,1,self.max)
-        wks1.write(5,1,self.med)
+        
+        wks1.write(0,0,'elem')
+        wks1.write(1,0,'mean')
+        wks1.write(2,0,'std')
+        wks1.write(3,0,'sum')
+        wks1.write(4,0,'min')
+        wks1.write(5,0,'max')
+        wks1.write(6,0,'med')
+        
+        for i, e in enumerate(self.msdata.isotopes):
+            
+            wks1.write(0,i+1,e)
+            wks1.write(1,i+1,self.stats_dict[e]['mean'])
+            wks1.write(2,i+1,self.stats_dict[e]['std'])
+            wks1.write(3,i+1,self.stats_dict[e]['sum'])
+            wks1.write(4,i+1,self.stats_dict[e]['min'])
+            wks1.write(5,i+1,self.stats_dict[e]['max'])
+            wks1.write(6,i+1,self.stats_dict[e]['med'])
 
         imgdata=io.BytesIO()
         self.fig.savefig(imgdata, format='png')
-        wks1.insert_image(0,3, '', {'image_data': imgdata})
+        wks1.insert_image(8,3, '', {'image_data': imgdata})
 
         workbook.close()
+        
+
+
 
 
 
